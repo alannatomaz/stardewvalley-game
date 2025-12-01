8866e7481009b95c1c8b016ad633414e174afb41
alannatomaz <alannatomaz2@gmail.com>
2025-12-01 18:07:15 -0300

    commit complete game

--- js/script.js
+++ js/script.js
@@ -11,14 +11,18 @@
 const body = document.querySelector("body");
 const game = document.querySelector(".game");
 
-const counter = document.querySelector("h1");
-const reset = document.querySelector(".reset");
+const count = document.querySelector("h1");
+const reset = document.querySelector("#reset");
 
 const ash = document.querySelector("#ash");
 
-const charmander = document.querySelector("#Serpent");
-const pikachu = document.querySelector("#Skeleton");
-const zubat = document.querySelector("#Crib");
+const Serpent = document.querySelector("#Serpent");
+const Skeleton = document.querySelector("#Skeleton");
+const Crib = document.querySelector("#Crib");
+
+let findSerpent = false;
+let findSkeleton = false;
+let findCrab = false;
 
 const audio = document.querySelector("audio");
 audio.volume = 0.4; //volume de 0 a 1
@@ -29,13 +33,58 @@ musicControl.addEventListener("click", (event) => {
     event.stopPropagation()
 
     const isOn = event.target.src.includes("on.png");
-    
+
     event.target.src = isOn
         ? "./assets/icons/off.png"
         : "./assets/icons/on.png";
 
     isOn ? audio.pause() : audio.play();
 });
+
+reset.addEventListener("click", () => {
+    window.location.reload();
+    reset.style.display = "none";
+})
+
+function clearCharactersAndFinishGame() {
+    Serpent.style.display = "none";
+    Skeleton.style.display = "none";
+    Crab.style.display = "none";
+    ash.style.display = "none";
+
+    reset.style.display = "block";
+    count.textCount = "";
+}
+
+let currentCount = 60;
+
+const interval = setInterval(() => {
+    if (currentCount <= 0) {
+        game.style.backgroundImage = "url('../assets/mary keller.jpg')";
+
+        clearCharactersAndFinishGame();
+        clearInterval(interval);
+        return;
+    }
+
+    currentCount--;
+    count.textContent = currentCount;
+}, 1000);
+
+function finishGame() {
+    if (findSerpent && findSkeleton && findCrab) {
+        clearCharactersAndFinishGame();
+
+        const timeOut = setTimeout(() => {
+            game.style.backgroundImage = "url('../assets/linnus-ending.jpg')";
+
+            clearInterval(interval);
+            clearTimeout(timeOut);
+
+            audio.pause();
+        }, 800);
+    }
+}
 
 function getRightPosition() { //pega a posição da direita para o personagem nao ultrapassar os limites da imagem de fundo
     return parseInt(ash.style.right.split("px")) || 2;
@@ -44,7 +93,47 @@ function getTopPosition() {
     return parseInt(ash.style.top.split("px")) || 2;
 }
 
-function verifyLookMonster() {
+function verifyLookMonster(to) {
+    finishGame();
+
+    const monsterRightPosition =
+        to === "ArrowLeft"
+            ? `${getRightPosition() - 64}px`
+            : `${getRightPosition() + 64}px`
+
+    if (findSerpent) {
+        const newTopPosition =
+            to = "ArrowUp"
+                ? `${getTopPosition() + 8}px`
+                : `${getTopPosition() - 8}px`
+
+        Serpent.style.top = newTopPosition;
+        Serpent.style.right = monsterRightPosition;
+
+    }
+
+    if (findSkeleton) {
+        const newTopPosition =
+            to = "ArrowUp"
+                ? `${getTopPosition() + 36}px`
+                : `${getTopPosition() - 36}px`
+
+        Skeleton.style.top = newTopPosition;
+        Skeleton.style.right = monsterRightPosition;
+
+    }
+
+    if (findCrab) {
+        const newTopPosition =
+            to = "ArrowUp"
+                ? `${getTopPosition() + 72}px`
+                : `${getTopPosition() - 72}px`
+
+        Crab.style.top = newTopPosition;
+        Crab.style.right = monsterRightPosition;
+
+    }
+
     if (
         getTopPosition() >= 2 &&
         getTopPosition() <= 98 &&
@@ -52,6 +141,7 @@ function verifyLookMonster() {
         getRightPosition() <= 216
     ) {
         Serpent.style.display = "block";
+        findSerpent = true;
         return;
     }
 
@@ -62,6 +152,7 @@ function verifyLookMonster() {
         getRightPosition() >= 42
     ) {
         Crab.style.display = "block";
+        findCrab = true;
         return;
     }
 
@@ -72,6 +163,7 @@ function verifyLookMonster() {
         getRightPosition() <= 650
     ) {
         Skeleton.style.display = "block";
+        findSkeleton = true;
         return;
     }
 }
